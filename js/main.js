119553687c7bb3e6e5723b0f260071a206efa6e8
dheeraj-komandur <dheerajkomandur@gmail.com>
2020-09-22 19:35:26 +0530

    Added SIH and RP

--- js/main.js
+++ js/main.js
@@ -270,7 +270,7 @@
    ------------------------------------------------------- */ 
 	var pxShow = 300; // height on which the button will show
 	var fadeInTime = 400; // how slow/fast you want the button to show
-	var fadeOutTime = 400; // how slow/fast you want the button to hide
+	var fadeOutTime = 100; // how slow/fast you want the button to hide
 	var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
 
    // Show or hide the sticky footer button
